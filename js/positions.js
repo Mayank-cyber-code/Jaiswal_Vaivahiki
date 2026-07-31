cf9e19c9ef27f86c9690a744dad4a882d039739e
Mayank-cyber-code <mayankkumarmzc@gmail.com>
2026-07-31 15:43:46 +0530

    Refactor POSITIONS object for layout adjustments

--- js/positions.js
+++ js/positions.js
@@ -4,77 +4,77 @@ const POSITIONS = {
             PERSONAL DETAILS
     ==============================*/
 
-    previewName:           { left: 235, top: 292, width: 300, fontSize: 22, fontWeight: 650 },
-    previewDOB:            { left: 235, top: 337, width: 300, fontSize: 19, fontWeight: 625 },
-    previewTime:           { left: 235, top: 386, width: 300, fontSize: 19, fontWeight: 625 },
-    previewPlace:          { left: 235, top: 438, width: 300, fontSize: 19, fontWeight: 600 },
-    previewHeight:         { left: 235, top: 485, width: 300, fontSize: 19, fontWeight: 600 },
-    previewComplexion:     { left: 235, top: 535, width: 300, fontSize: 19, fontWeight: 600 },
-    previewMaritalStatus:  { left: 235, top: 584, width: 300, fontSize: 19, fontWeight: 600 },
-    previewCaste:          { left: 235, top: 632, width: 300, fontSize: 19, fontWeight: 600 },
-    previewSubCaste:       { left: 235, top: 680, width: 300, fontSize: 19, fontWeight: 600 },
-    previewManglik:        { left: 235, top: 725, width: 300, fontSize: 19, fontWeight: 600 },
-    previewMotherTongue:   { left: 235, top: 775, width: 300, fontSize: 19, fontWeight: 600 },
-    previewDiet:           { left: 235, top: 820, width: 300, fontSize: 19, fontWeight: 600 },
-    previewHobbies:        { left: 235, top: 867, width: 300, fontSize: 19, fontWeight: 600 },
-    previewOther:          { left: 235, top: 910, width: 300, fontSize: 19, fontWeight: 600 },
+    previewName:           { left: 225, top: 292, width: 300, fontSize: 21, fontWeight: 620 },
+    previewDOB:            { left: 225, top: 337, width: 300, fontSize: 20, fontWeight: 620 },
+    previewTime:           { left: 225, top: 382, width: 300, fontSize: 20, fontWeight: 620 },
+    previewPlace:          { left: 225, top: 425, width: 300, fontSize: 20, fontWeight: 600 },
+    previewHeight:         { left: 225, top: 468, width: 300, fontSize: 20, fontWeight: 600 },
+    previewComplexion:     { left: 225, top: 515, width: 300, fontSize: 20, fontWeight: 600 },
+    previewMaritalStatus:  { left: 225, top: 560, width: 300, fontSize: 20, fontWeight: 600 },
+    previewCaste:          { left: 225, top: 603, width: 300, fontSize: 20, fontWeight: 600 },
+    previewSubCaste:       { left: 225, top: 645, width: 300, fontSize: 20, fontWeight: 600 },
+    previewManglik:        { left: 225, top: 690, width: 300, fontSize: 20, fontWeight: 600 },
+    previewMotherTongue:   { left: 225, top: 736, width: 300, fontSize: 20, fontWeight: 600 },
+    previewDiet:           { left: 225, top: 780, width: 300, fontSize: 20, fontWeight: 600 },
+    previewHobbies:        { left: 225, top: 825, width: 300, fontSize: 20, fontWeight: 600 },
+    previewOther:          { left: 225, top: 875, width: 300, fontSize: 20, fontWeight: 600 },
 
     /*==============================
             CONTACT
     ==============================*/
 
-    previewMobile:         { left: 730, top: 860, width: 250, fontSize: 22, fontWeight: 650 },
-    previewRelation:       { left: 730, top: 905, width: 250, fontSize: 22, fontWeight: 600 },
+    previewMobile:         { left: 730, top: 825, width: 250, fontSize: 22, fontWeight: 650 },
+    previewRelation:       { left: 730, top: 870, width: 250, fontSize: 22, fontWeight: 600 },
 
     /*==============================
             EDUCATION
     ==============================*/
 
-    previewHighestQualification:{ left: 235, top: 1022, width: 270, fontSize: 18, fontWeight: 600 },
-    previewCollege:             { left: 235, top: 1055, width: 270, fontSize: 18, fontWeight: 600 },
-    previewSchool10:            { left: 235, top: 1090, width: 270, fontSize: 18, fontWeight: 600 },
-    previewSchool12:            { left: 235, top: 1125, width: 270, fontSize: 18, fontWeight: 600 },
-    previewOccupation:          { left: 235, top: 1158, width: 270, fontSize: 18, fontWeight: 600 },
-    previewIncome:              { left: 235, top: 1190, width: 270, fontSize: 18, fontWeight: 600 },
+    previewHighestQualification:{ left: 225, top: 997, width: 270, fontSize: 20, fontWeight: 600 },
+    previewCollege:             { left: 225, top: 1040, width: 270, fontSize: 20, fontWeight: 600 },
+    previewSchool12:            { left: 225, top: 1080, width: 270, fontSize: 20, fontWeight: 600 },
+    previewSchool10:            { left: 225, top: 1120, width: 270, fontSize: 20, fontWeight: 600 },
+    previewOccupation:          { left: 225, top: 1160, width: 270, fontSize: 20, fontWeight: 600 },
+    previewIncome:              { left: 225, top: 1200, width: 270, fontSize: 20, fontWeight: 600 },
 
     /*==============================
             FAMILY
     ==============================*/
 
-    previewFatherName:       { left: 715, top: 1023, width: 250, fontSize: 18, fontWeight: 600 },
-    previewFatherOccupation: { left: 715, top: 1055, width: 250, fontSize: 18, fontWeight: 600 },
-    previewMotherName:       { left: 715, top: 1090, width: 250, fontSize: 18, fontWeight: 600 },
-    previewMotherOccupation: { left: 715, top: 1125, width: 250, fontSize: 18, fontWeight: 600 },
-    previewBrothers:         { left: 715, top: 1158, width: 250, fontSize: 18, fontWeight: 600 },
-    previewSisters:          { left: 715, top: 1195, width: 250, fontSize: 18, fontWeight: 600 },
+    previewFatherName:       { left: 700, top: 997, width: 250, fontSize: 20, fontWeight: 600 },
+    previewFatherOccupation: { left: 700, top: 1040, width: 250, fontSize: 20, fontWeight: 600 },
+    previewMotherName:       { left: 700, top: 1080, width: 250, fontSize: 20, fontWeight: 600 },
+    previewMotherOccupation: { left: 700, top: 1120, width: 250, fontSize: 20, fontWeight: 600 },
+    previewBrothers:         { left: 700, top: 1160, width: 250, fontSize: 20, fontWeight: 600 },
+    previewSisters:          { left: 700, top: 1200, width: 250, fontSize: 20, fontWeight: 600 },
 
     /*==============================
             PARTNER
     ==============================*/
 
-    previewPartnerProfession:{ left: 235, top: 1285, width: 300, fontSize: 18, fontWeight: 600 },
-    previewPartnerLocation:  { left: 235, top: 1315, width: 300, fontSize: 18, fontWeight: 600 },
-    previewPartnerOther:     { left: 235, top: 1345, width: 300, fontSize: 18, fontWeight: 600 },
+    previewPartnerProfession:{ left: 220, top: 1295, width: 300, fontSize: 20, fontWeight: 600 },
+    previewPartnerLocation:  { left: 220, top: 1328, width: 300, fontSize: 20, fontWeight: 600 },
+    previewPartnerOther:     { left: 220, top: 1360, width: 300, fontSize: 20, fontWeight: 600 },
 
     /*==============================
             ADDRESS
     ==============================*/
 
-    previewCurrentAddress:   { left: 710, top: 1285, width: 250, fontSize: 18, fontWeight: 600 },
-    previewPermanentAddress: { left: 710, top: 1315, width: 250, fontSize: 18, fontWeight: 600 },
-    previewDistState:        { left: 710, top: 1345, width: 250, fontSize: 18, fontWeight: 600 },
+    previewCurrentAddress:   { left: 700, top: 1295, width: 250, fontSize: 20, fontWeight: 600 },
+    previewPermanentAddress: { left: 700, top: 1328, width: 250, fontSize: 20, fontWeight: 600 },
+    previewDistState:        { left: 700, top: 1360, width: 250, fontSize: 20, fontWeight: 600 },
 
     /*==============================
             DECLARATION
     ==============================*/
 
-    previewSenderName:       { left: 780, top: 1420, width: 200, fontSize: 18, fontWeight: 600 },
-    previewSenderMobile:     { left: 780, top: 1460, width: 200, fontSize: 18, fontWeight: 600 },
+    previewSenderName:       { left: 810, top: 1433, width: 200, fontSize: 16, fontWeight: 550 },
+    previewSenderMobile:     { left: 810, top: 1473, width: 200, fontSize: 16, fontWeight: 550 },
 
     /*==============================
             PHOTO
     ==============================*/
 
-    previewPhoto:            { left: 475, top: 270, width: 450, height: 580 }
+    previewPhoto:            { left: 500, top: 250, width: 450, height: 575 }
 
 };
